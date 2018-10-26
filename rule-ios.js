059127837927d5f09e2a08aca4a662b6df8014e5
jewdore <jerry.dong@upltv.com>
2018-10-26 14:00:32 +0800

    rule ios one

--- rule-ios.js
+++ rule-ios.js
@@ -1,4 +1,22 @@
-function getInfo(email_body){
-   return email_body + 'success';
+      function getInfo(email_body) {
 
-}
+        var d=email_body.replace(/(\r\n)|(\n)/g,'');
+
+        var object = {};
+
+        var img = /img\s*src=\"(.*?)\".*?alt=\"(.*?)\"/.exec(d)
+        object['icon'] = img[1]
+        object['app_name'] = img[2]
+        object['goods_name'] = ""
+
+        if (object['app_name'].indexOf(", ")) {
+          var app_goods = object['app_name'].split(", ")
+          object['app_name'] = app_goods[0]
+          object['goods_name'] = app_goods[1]
+        }
+
+        var money = /<span\s+style=\"font-weight:600;white-space:nowrap\">(.{1})(.*?)<\/span>/.exec(d)
+        object['money_unit'] = money[1]
+        object['money'] = money[2]
+        block(JSON.stringify(object))
+      }
