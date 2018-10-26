1ec147633a6e906c2a737dda9d9c86b0e9476c65
jewdore <jerry.dong@upltv.com>
2018-10-26 14:20:53 +0800

    rule ios 判空

--- rule-ios.js
+++ rule-ios.js
@@ -5,18 +5,26 @@
         var object = {};
 
         var img = /img\s*src=\"(.*?)\".*?alt=\"(.*?)\"/.exec(d)
-        object['icon'] = img[1]
-        object['app_name'] = img[2]
-        object['goods_name'] = ""
 
-        if (object['app_name'].indexOf(", ")) {
-          var app_goods = object['app_name'].split(", ")
-          object['app_name'] = app_goods[0]
-          object['goods_name'] = app_goods[1]
+        if(img && img.length > 2){
+          object['icon'] = img[1] 
+          object['app_name'] = img[2]
+
+          if (object['app_name'].indexOf(", ")) {
+            var app_goods = object['app_name'].split(", ")
+
+            object['app_name'] =  app_goods[0]
+            object['goods_name'] = app_goods[1]
+          }
         }
 
+        
         var money = /<span\s+style=\"font-weight:600;white-space:nowrap\">(.{1})(.*?)<\/span>/.exec(d)
-        object['money_unit'] = money[1]
-        object['money'] = money[2]
+
+        if(money && money.length > 2){
+          object['money_unit'] = money[1] 
+          object['money'] = money[2]
+        }
+        
         block(JSON.stringify(object))
       }
